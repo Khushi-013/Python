954809e3c0f0797d6a7eef0f8f0e4fc18e2ea9ca
Khushi Sinha <khushi.211583201@vcet.edu.in>
2024-11-05 20:05:48 +0530

    initials

---
+++ initials.py
@@ -0,0 +1,9 @@
+# Write code below 💖
+
+print("K   K    SSSS")
+print("K  K    S    ")
+print("K K     S    ")
+print("K        SSSS")
+print("K K          S")
+print("K  K         S")
+print("K   K   SSSS")
